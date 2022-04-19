0c6731c12204d3822ee3a98222f44d0259e9283b
Md Shahriar Islam <mdshahriarislam23@gmail.com>
2022-04-19 16:46:58 +0600

    reviews section added

--- src/Components/Home/Reviews/Reviews.js
+++ src/Components/Home/Reviews/Reviews.js
@@ -1,48 +1,49 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import { AiFillStar } from 'react-icons/ai';
+import review from "../../../images/banner/review.jpg"
 
 const Reviews = () => {
     return (
-        <div className="background">
-            <h3 className="text-center mb-2 mt-5">SOME OF MY STUDENTS STORY</h3>
-            <Carousel variant="dark">
-                <Carousel.Item>
+        <div className="mx-4">
+            <h4 className="text-center m-4">SOME OF MY STUDENTS FEELINGS</h4>
+            <div className='card-holder' >
 
-                    <p className="d-block mx-auto w-25  text-center p-5">
-                        <strong className="p-2" >Robert downey jr</strong><br></br>
-                        He is very nice teacher He teaches Us very carefully.the dedication he has is mind blowing.His every courses are well designed.I was a student of his academic combo pack.Now I am also taking question bank solving course<br></br>
-                        <AiFillStar />
-                        <AiFillStar />
-                        <AiFillStar />
-                        <AiFillStar />
-                        <AiFillStar />
-                    </p>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <p className="d-block mx-auto w-25  text-center p-5">
-                        <strong className="p-2" >Bruce Wayne</strong><br></br>
-                        He is a wonderful teacher He teaches Us very carefully.I am a student of his Admission Program.His every courses are well designed.I was a student of his academic combo pack.Now I am also taking question bank solving course<br></br>
-                        <AiFillStar />
-                        <AiFillStar />
-                        <AiFillStar />
-                        <AiFillStar />
-                        <AiFillStar />
-                    </p>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <p className="d-block mx-auto w-25  text-center p-5">
-                        <strong className="p-2" >Steve Rogers</strong><br></br>
-                        He is a wonderful teacher He teaches Us very carefully.I am a student of his Admission Program.His every courses are well designed.I was a student of his academic combo pack.Now I am also taking question bank solving course<br></br>
+                <div>
+                    <h3 class="text-center">Robert downey jr</h3>
+                    <p style={{ textAlign: 'justify' }}>He is very nice teacher He teaches Us very carefully.the dedication he has is mind blowing.His every courses are well designed.I was a student of his academic combo pack.Now I am also taking question bank solving course<br></br>
                         <AiFillStar />
                         <AiFillStar />
                         <AiFillStar />
                         <AiFillStar />
                         <AiFillStar /></p>
-                </Carousel.Item>
-            </Carousel>
+                </div>
+                <div>
+                    <h3 className="text-center">Bruce Wayne</h3>
+                    <p style={{ textAlign: 'justify' }}>He is a wonderful teacher He teaches Us very carefully.I am a student of his Admission Program.His every courses are well designed.I was a student of his academic combo pack.Now I am also taking question bank solving course<br></br>
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar /></p>
+                </div>
+                <div>
+                    <h3 className="text-center">Steve Rogers
+                    </h3>
+                    <p style={{ textAlign: 'justify' }}> He is a wonderful teacher He teaches Us very carefully.I am a student of his Admission Program.His every courses are well designed.I was a student of his academic combo pack.Now I am also taking question bank solving course<br></br>
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar /></p>
+                </div>
+
+
+
+            </div>
 
         </div>
+
     );
 };
 
